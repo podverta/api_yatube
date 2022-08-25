8a4cdc8ac1cde9904cd3a5abd4b80dd84946b906
Podverta <podverta@yandex.ru>
2022-08-25 16:54:40 +0300

    1 part

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,17 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import DefaultRouter
 from .views import APIPost
+
+router = DefaultRouter()
+router.register('posts', APIPost, basename='posts')
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(),
          name='token_refresh'),
-    path('posts/', APIPost.as_view({'get': 'list'})),
 ]
+
+urlpatterns += router.urls
--- api/views.py
+++ api/views.py
@@ -2,9 +2,49 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from django.shortcuts import get_object_or_404
 
 class APIPost(viewsets.ViewSet):
     queryset = Post.objects.all()
     def list(self, request):
         serializer = PostSerializer(self.queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk):
+        item = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(item)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk):
+        item = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(item, data=request.data)
+        if serializer.is_valid():
+            if item.author == request.user:
+                serializer.save(author=request.user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def partrial_update(self, request, pk):
+        item = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(item, data=request.data, partial=True)
+        if serializer.is_valid():
+            if item.author == request.user:
+                serializer.save(author=request.user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk):
+        item = get_object_or_404(Post, pk=pk)
+        if item.author == request.user:
+           item.delete()
+           return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_403_FORBIDDEN)
