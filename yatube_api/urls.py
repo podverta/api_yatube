4b75416dd56a1b47660372bd51d9b6138aa5b192
podverta <podverta@yandex.ru>
2022-08-25 07:32:14 +0300

    first commit

---
+++ yatube_api/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('redoc/', TemplateView.as_view(template_name='redoc.html'), name='redoc'),
+]
